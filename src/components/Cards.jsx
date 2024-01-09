96b90832850eec2983c9280b72e73994d1073a05
Euardo Rojas <edurojas@edurojas.tech>
2024-01-09 17:41:06 -0300

    refactor: atualização em cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,10 +1,8 @@
-import Yoda from "../assets/yoda.png";
-import img02 from "../assets/2.webp"
-import img03 from "../assets/3.webp"
-import img04 from "../assets/4.webp"
-import CardCustom from "./CardCustom";
+
+import SchemaImages from "../db/SchemaImages";
 
 function Cards() {
+  const { Yoda, img02, img03, img04 } = SchemaImages
   return (
     <div className="container">
       <div className="row text-center text-lg-start">
---
+++ src/db/SchemaImages.js
@@ -0,0 +1,6 @@
+import Yoda from "../assets/yoda.png";
+import img02 from "../assets/2.webp"
+import img03 from "../assets/3.webp"
+import img04 from "../assets/4.webp"
+
+export default { Yoda, img02, img03, img04 }
